2c436c9678f2bca1b6f87c9146960d915062b0aa
it21312908 <it21312908@my.sliit.lk>
2024-10-20 12:27:39 +0530

    modified

--- categorycontroller.cs
+++ categorycontroller.cs
@@ -12,10 +12,7 @@ namespace BreezeBuy.Controllers
     {
         private readonly CategoryService _categoryService;
 
-        public CategoryController(CategoryService categoryService)
-        {
-            _categoryService = categoryService;
-        }
+      
 
         // POST: api/category
         [HttpPost]
